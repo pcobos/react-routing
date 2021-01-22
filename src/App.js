6bf6bed9dcd0c302e5dff3f172f35315735a6696
Pedro Cobos <pedro.cobos@live.com>
2021-01-21 23:25:00 -0600

    feat: styled navbar andadded Route components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import '/components/About';
-import '/components/Shop';
-import '/components/Nav';
+import Nav from './components/Nav';
+import About from './components/About';
+import Shop from './components/Shop';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Nav /> */}
-      <About />
-      <Shop />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/shop" component={Shop}/>
+      </div>
+    </Router>
   );
 }
 
+const Home = () => {
+  return(
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
 export default App;
+
