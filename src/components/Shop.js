b79fe88f0125582400e929be1bd08ad2f3549fcb
Pedro Cobos <pedro.cobos@live.com>
2021-01-22 22:34:22 -0600

    feat: add link functionality to every item

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import React, { Link } from 'react-router-dom';
 
 const Shop = () => {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+  
+  const [cards, setCards] = useState([]); 
+
+  const fetchItems = async () => {
+    const esCardsApi = "https://api.elderscrollslegends.io/v1/cards"
+
+    const data = await fetch(esCardsApi);
+    const items = await data.json();
+    // const slicedItems = items.cards.slice(0, 10);
+    const slicedItems = items.cards.slice(0, 10);
+    setCards(slicedItems);
+  }
+  
+
   return(
     <div>
       <h1>Shop</h1>
+      {cards.map(card => (
+        <Link><h1>{card.name}</h1></Link>
+      ))}
     </div>
   )
 }
