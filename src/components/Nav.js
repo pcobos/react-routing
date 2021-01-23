fd47e6697a8afcc5fce7f2558b17766fded09779
Pedro Cobos <pedro.cobos@live.com>
2021-01-22 18:30:09 -0600

    feat: add style to nav items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
+  const style = {
+    color: 'white',
+    textDecoration: 'none'
+  };
+
   return(
     <nav>
       <h3>Logo</h3>
       <ul className="nav-links">
         {/* In order to turn <li> JSX elements we need to wrap them in the*/}
-        <Link path="/about">
+        <Link style={style} to="/about">
           <li>About</li>
         </Link>
-        <Link path="/shop">
+        <Link style={style} to="/shop">
           <li>Shop</li>
         </Link>
       </ul>
